097cf696caeedf7187e3d63c7137edd0bd6992ae
Mikhail Semenov <18450042+mikkerlo@users.noreply.github.com>
2022-09-25 00:46:07 +0200

    Remove socket systems, migrate to updateEmbededDocuments
    Updating manifest and workflow also

--- scripts/module.js
+++ scripts/module.js
@@ -1,58 +1,49 @@
-Hooks.on("ready", () =>{
-    game.socket.on('module.self-player-init', async data => {
-        console.log(data);
-        if (game.user.isGM && data.event == "set-initiative") {
-            let available_combats = game.combats.filter(combat => combat.scene.id == data.token.sceneId);
-            if (available_combats.length == 0) {
+Hooks.on("chatCommandsReady", chatCommands => chatCommands.registerCommand(
+    chatCommands.createCommandFromData({
+        commandKey: "/init",
+        invokeOnCommand: (chatlog, messageText, chatdata) => {
+            logger.debug("[fvtt-token-self-init] Invoked /init");
+            logger.debug("[fvtt-token-self-init] " + messageText);
+            let initiative = parseFloat(messageText);
+            if (Number.isNaN(initiative)) {
+                ui.notifications.error("[fvtt-token-self-init] Error while parsing initiative");
+                logger.error("[fvtt-token-self-init] Error while parsing initiative from chat " + messageText);
                 return;
             }
 
-            let combat = available_combats[0];
-            await Promise.all(
-                combat.combatants
-                .filter(combatan => combatan.token.id == data.token.id)
-                .map(c => c.update({initiative: data.initiative})));
-        }
-    });
-}); 
+            let token_list = canvas.tokens.controlled.filter(token => token.inCombat);
+            if (token_list.length == 0) {
+                token_list = canvas.tokens.ownedTokens
+                    .filter(token => token.inCombat)
+                    .filter(c =>
+                        c.actor.items
+                            .filter(item => item.name == "SelfInitIgnore")
+                            .length == 0);
+            }
 
-Hooks.on("chatCommandsReady", function(chatCommands) {
-    chatCommands.registerCommand(chatCommands.createCommandFromData({
-      commandKey: "/init",
-      invokeOnCommand: (chatlog, messageText, chatdata) => {
-        console.log("Invoked /init");
-        console.log(messageText);
-        let initiative = parseFloat(messageText);
-        if (Number.isNaN(initiative)) {
-            console.log("Error while parsing initiative");
-            return;
+            if (token_list.length != 1) {
+                ui.notifications.warn("[fvtt-token-self-init] Multipple tokens detected, aborting the operation");
+                return;
+            }
+
+            const tokenToCombant = Array.from(game.combats)
+                .flatMap(c => Array.from(c.combatants))
+                .map(c => ({key: c.tokenId, val: c.id}))
+                .reduce((map, obj) => map.set(obj.key, obj.val), new Map)
+
+            const initiatives = token_list
+                .filter(token => token.isOwner)
+                .map(player_token => ({
+                    "_id": tokenToCombant.get(player_token.id),
+                    "initiative": initiative
+                }));
+
+            try {
+                game.combat.updateEmbeddedDocuments("Combatant", initiatives);
+            } catch (err) {
+                logger.error("Error while trying to update initiatives");
+                logger.error(err);
+            }
         }
-          
-        let token_list = canvas.tokens.controlled.filter(token => token.inCombat);
-        if (token_list.length == 0) {
-            token_list = canvas.tokens.ownedTokens.filter(token => token.inCombat).filter(c => c.actor.items.filter(item => item.name == "SelfInitIgnore").length == 0);
-        }
-          
-        if (token_list.length != 1) {
-            return;
-        }
-        
-        return token_list.filter(token => token.isOwner).map(player_token => {
-            game.socket.emit('module.self-player-init', {
-                event: "set-initiative",
-                token: {
-                    id: player_token.id,
-                    sceneId: player_token.scene.id,
-                  },
-                initiative: initiative,
-            });
-        });
-        
-      },
-      shouldDisplayToChat: false,
-      createdMessageType: 1,
-      iconClass: "fa-sticky-note",
-      description: "Set initiative for selected or owned tokens"
-    }));
-  });
-  
+    })
+));
